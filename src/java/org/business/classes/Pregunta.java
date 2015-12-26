78087d32298c57326784b28fffe4b3e558bb96bb
alwuud <dahh.drag@gmail.com>
2015-12-26 11:34:49 -0600

    cambio en los Inserts de Preguntas

--- src/java/org/business/classes/Pregunta.java
+++ src/java/org/business/classes/Pregunta.java
@@ -5,7 +5,12 @@
  */
 package org.business.classes;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import org.database.DataBaseConnection;
 
 /**
  *
@@ -103,7 +108,120 @@ public class Pregunta {
     }
     
     
-    public 
+    
+    public boolean insertQuestionsDB(int indicePregunta) throws SQLException{
+        
+        Connection dbConnection = null;
+        PreparedStatement preparedStatement = null;
+
+        String insertTableSQL = "INSERT INTO RESPUESTA"
+                + "(res,correcta, id_pregunta) VALUES"
+                + "(?,?,?)";
+
+        try {
+            dbConnection = new DataBaseConnection().getDBConnection();
+            preparedStatement = dbConnection.prepareStatement(insertTableSQL);
+            
+            for(Respuesta actual: respuestas)
+            {
+                 preparedStatement.setString(1, actual.getRes());
+                 preparedStatement.setBoolean(2, actual.isCorrecta());
+                 preparedStatement.setInt(3, indicePregunta);
+                 preparedStatement.executeUpdate();
+                 
+                 
+                
+                
+            }
+            
+           dbConnection.close();
+          
+            
+          
+
+            // execute insert SQL stetement
+            
+            
+            
+            
+
+            return true;
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+            return false;
+
+        } finally {
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (dbConnection != null) {
+                dbConnection.close();
+            }
+
+        }
+        
+    
+        
+    }
+    public boolean insertDB() throws SQLException{
+               Connection dbConnection = null;
+        PreparedStatement preparedStatement = null;
+
+        String insertTableSQL = "INSERT INTO PREGUNTA"
+                + "(texto,tiempo, id_curso, id_dificultad) VALUES"
+                + "(?,?,?,?) RETURNING id_pregunta";
+
+        try {
+            dbConnection = new DataBaseConnection().getDBConnection();
+            preparedStatement = dbConnection.prepareStatement(insertTableSQL);
+
+            preparedStatement.setString(1, question);
+            preparedStatement.setString(2,tiempo );
+            preparedStatement.setInt(3, curso);
+            preparedStatement.setInt(4, dificultad);
+            
+          
+            ResultSet rs;
+            rs= preparedStatement.executeQuery();
+            int indicePregunta=0;
+            while(rs.next()){
+                indicePregunta= rs.getInt("id_pregunta");
+                
+            }
+            
+            
+            this.insertQuestionsDB(indicePregunta);
+            // execute insert SQL stetement
+           
+            dbConnection.close();
+            
+            
+            
+            
+
+            return true;
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+            return false;
+
+        } finally {
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (dbConnection != null) {
+                dbConnection.close();
+            }
+
+        }
+    }
     
     
 }
